44d7c4b7e92a2fc785443a77260a495ab28035a7
malena-3 <mlodi1@go.pasadena.edu>
2024-07-09 15:58:04 -0700

    finished directions

--- App.js
+++ App.js
@@ -1,5 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Image } from 'react-native';
+import { useState } from 'react';
+
 
 import Button from './components/Button';
 import ImageViewer from './components/ImageViewer';
@@ -8,6 +10,8 @@ import * as ImagePicker from 'expo-image-picker';
 const PlaceholderImage = require('./assets/images/MalenaCindy.jpg');
 
 export default function App() {
+  const [selectedImage, setSelectedImage] = useState(null);
+
   const pickImageAsync = async () => {
     let result = await ImagePicker.launchImageLibraryAsync({
       allowsEditing: true,
@@ -15,7 +19,7 @@ export default function App() {
     });
 
     if (!result.canceled) {
-      console.log(result);
+      setSelectedImage(result.assets[0].uri);
     } else {
       alert('You did not select any image.');
     }
@@ -24,7 +28,10 @@ export default function App() {
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
-      <ImageViewer placeholderImageSource={PlaceholderImage} />
+      <ImageViewer
+          placeholderImageSource={PlaceholderImage}
+          selectedImage={selectedImage}
+        />
       </View>
       <View style={styles.footerContainer}>
         <Button theme="primary" label="Choose a photo" onPress={pickImageAsync}/>
